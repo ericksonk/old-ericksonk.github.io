c0e69d722768b25b8e6962fe03e865b879d8da1e
Kristine Erickson <ericksonk09@gmail.com>
2015-10-11 22:23:18 -0400

    setting up character limit on visible blog list

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+// source: http://slickmedia.co.uk/blog/glenns-blog/limit-number-of-characters-in-div-jquery-css/
+$('li.posts p').each(function () {
+  var len = $(this).text().length; // selects p element's text and length
+
+});
+
+// `.text()` selects combined text contents of matched element
