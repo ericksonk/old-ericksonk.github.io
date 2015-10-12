dff4364a12562aa749332aab7641deceadefb0e2
Kristine Erickson <ericksonk09@gmail.com>
2015-10-11 22:47:48 -0400

    trying to add event listener to show entire blog

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,11 @@
 // source: http://slickmedia.co.uk/blog/glenns-blog/limit-number-of-characters-in-div-jquery-css/
 $('li.posts p').each(function () {
   var len = $(this).text().length; // selects p element's text and length
-  if ($(len > 140)) { // if length is greater than x
-    $(this).text($(this).text().substring(0,140) + '...'); // return `p` elements characters between 0 and x plux `...`
+  if ($(len > 300)) { // if length is greater than x
+    $(this).text($(this).text().substring(0, 300) + '...'); // return `p` elements characters between 0 and x plux `...`
+    $('a.readMore').on('click', function () {
+      $(this).show($(this).text().substring(300) + '...');
+    });
   }
 });
 
