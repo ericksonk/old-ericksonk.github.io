d2a76c3903b3abca37b461f5f5ba05774d0c610e
Kristine Erickson <ericksonk09@gmail.com>
2015-12-01 10:37:38 -0500

    added plugins.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,5 @@
-// source: http://slickmedia.co.uk/blog/glenns-blog/limit-number-of-characters-in-div-jquery-css/
-$('li.posts p').each(function () {
-  var len = $(this).text().length; // selects p element's text and length
-  if ($(len > 300)) { // if length is greater than x
-    $(this).text($(this).text().substring(0, 300) + '...'); // return `p` elements characters between 0 and x plux `...`
-
-  $('a.readMore').on('click', function () {
-    $('li.posts p').show($('li.posts p').text(.substring(300));
-  });
-  }
+$('#portfolio').on('click', function(){
+  $('html, body').animate({
+    scrollTop: $('#portfolio').offset().top
+  }, 'slow');
 });
-// `.text()` selects combined text contents of matched element
-// `.substring(indexStart, indexEnd)` - returns part of string between the given indexes
