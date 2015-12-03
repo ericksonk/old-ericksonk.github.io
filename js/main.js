03c38e4ef62c2a19ee60ffa51a3416d219d09565
Kristine Erickson <ericksonk09@gmail.com>
2015-12-03 11:43:25 -0500

    slow page scroll

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
-
+  console.log('hi kristine :)')
+  $(".show-home").click(function(){
+    $('html, body').animate({
+        scrollTop: $("#k-home").offset().top},
+        'slow');
+  });
+  $(".folio").click(function(){
+    $('html, body').animate({
+        scrollTop: $("#k-portfolio").offset().top},
+        'slow');
+  });
+  $(".contact-me").click(function(){
+    $('html, body').animate({
+        scrollTop: $("#k-contact").offset().top},
+        'slow');
+  });
 });
